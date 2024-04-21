bbb983b2d29e7a3f408fd35bcbb75a21e5da4eed
Gguidini <giovanni@codecov.io>
2024-04-21 18:45:51 +0200

    add simple in-memory tasks

--- src/App.svelte
+++ src/App.svelte
@@ -8,6 +8,8 @@
     getPaddedMinutesFromTimeInSeconds,
     getPaddedSecondsFromTimeInSeconds,
   } from "./utils/clockUtils";
+  import SubTitle from "./lib/SubTitle.svelte";
+  import Tasks from "./lib/Tasks.svelte";
 
   const CLOCK_INFO: ClockInfo[] = [
     { name: "focus_time", durationSeconds: 60 * 25 },
@@ -51,55 +53,70 @@
   }
 </script>
 
-<main class="font-mono flex flex-col items-strech justify-items-center">
-  <img
-    src={pomotimer}
-    alt="Pomodoro timer"
-    style="width: 128px; height: 128px;"
-    class="self-center"
-  />
-  <h1 class="py-8 px-4 text-xl">Yet Another Pomodoro Timer</h1>
-
-  <h3 class="font-bold">Stats</h3>
-  <div class="flex items-center justify-between py-3" id="stats">
-    <p class="px-2">Focus time: {appContext.focusTime}s</p>
-    <p class="px-2">Pomodoros completed: {appContext.pomodorosCompleted}</p>
-    <p class="px-2">Pomodoros incomplete: {appContext.pomodorosIncomplete}</p>
+<main class="font-mono">
+  <div class="flex flex-col items-strech">
+    <img
+      src={pomotimer}
+      alt="Pomodoro timer"
+      style="width: 128px; height: 128px;"
+      class="self-center"
+    />
+    <h1 class="py-8 px-4 text-xl">Yet Another Pomodoro Timer</h1>
   </div>
 
-  <div id="clock-type-selector" class="py-4 flex justify-between">
-    {#each CLOCK_INFO as clockInfo, idx}
-      <div class="flex flex-col justify-center px-4">
-        <label>
-          <input
-            type="radio"
-            bind:group={currentClockIdx}
-            value={idx}
-            disabled={appContext.isClockRunning}
-          />
-          {clockInfo.name.replace("_", " ")}
-          ({getPaddedMinutesFromTimeInSeconds(
-            CLOCK_INFO[idx].durationSeconds
-          )}:{getPaddedSecondsFromTimeInSeconds(
-            CLOCK_INFO[idx].durationSeconds
-          )})
-        </label>
-        <input
-          bind:value={CLOCK_INFO[idx].durationSeconds}
-          type="number"
-          disabled={appContext.isClockRunning}
+  <div class="flex flex-col items-strech justify-evenly lg:flex-row">
+    <div
+      class="flex flex-col justify-start items-strech justify-items-center basis-1/2"
+      id="pomodoro"
+    >
+      <div id="clock-type-selector" class="py-4 flex justify-between">
+        {#each CLOCK_INFO as clockInfo, idx}
+          <div class="flex flex-col justify-center px-4">
+            <label>
+              <input
+                type="radio"
+                bind:group={currentClockIdx}
+                value={idx}
+                disabled={appContext.isClockRunning}
+              />
+              {clockInfo.name.replace("_", " ")}
+              ({getPaddedMinutesFromTimeInSeconds(
+                CLOCK_INFO[idx].durationSeconds
+              )}:{getPaddedSecondsFromTimeInSeconds(
+                CLOCK_INFO[idx].durationSeconds
+              )})
+            </label>
+            <input
+              bind:value={CLOCK_INFO[idx].durationSeconds}
+              type="number"
+              disabled={appContext.isClockRunning}
+            />
+          </div>
+        {/each}
+      </div>
+
+      <div class="py-8 px-4 border-2 border-indigo-900 shadow-lg" id="clock">
+        <Clock
+          durationSeconds={currentClockInfo.durationSeconds}
+          clockName={currentClockInfo.name}
+          on:clock_complete={handleClockComplete}
+          on:clock_started={handleClockStarted}
         />
       </div>
-    {/each}
-  </div>
 
-  <div class="py-8 px-4 border-2 border-indigo-900 shadow-lg" id="clock">
-    <Clock
-      durationSeconds={currentClockInfo.durationSeconds}
-      clockName={currentClockInfo.name}
-      on:clock_complete={handleClockComplete}
-      on:clock_started={handleClockStarted}
-    />
+      <SubTitle title="Stats" />
+      <div class="flex items-center justify-between py-3" id="stats">
+        <p class="px-2">Focus time: {appContext.focusTime}s</p>
+        <p class="px-2">Pomodoros completed: {appContext.pomodorosCompleted}</p>
+        <p class="px-2">
+          Pomodoros incomplete: {appContext.pomodorosIncomplete}
+        </p>
+      </div>
+    </div>
+
+    <div class="basis-1/2 flex justify-end" id="tasks">
+      <Tasks />
+    </div>
   </div>
 </main>
 
---
+++ src/lib/SubTitle.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  export let title: string;
+</script>
+
+<h3 class="font-bold border-b-2 border-indigo-900 shadow-lg mt-4">{title}</h3>
