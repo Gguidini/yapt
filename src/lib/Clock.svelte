9e8285e1459c2966f67269af9c10316172781a54
Gguidini <giovanni@codecov.io>
2024-04-21 17:10:09 +0200

    Several updates
    * Better track focus time, completed pomos;
    * Track incomplete pomos;
    * Remove dev_clock;
    * Change icon

--- src/lib/Clock.svelte
+++ src/lib/Clock.svelte
@@ -1,79 +1,76 @@
 <script lang="ts">
-    import { onDestroy } from 'svelte';
-    import Icon from './Icon.svelte';
+  import { onDestroy } from "svelte";
+  import Icon from "./Icon.svelte";
 
-    import { createEventDispatcher } from 'svelte';
-    import type { ClockName, ClockEvents } from '../types/clock';
-  
-    export let durationSeconds: number = 60 * 25; // Default is 25mins
-    export let clockName: ClockName;
-    let timeSpent = 0;
-    let playing: boolean = false;
+  import { createEventDispatcher } from "svelte";
+  import type { ClockName, ClockEvents } from "../types/clock";
 
-    let interval: number | undefined = undefined;
-    
-    $: timeLeft = durationSeconds - timeSpent;
-    $: minutes = ('0' + Math.floor(timeLeft / 60)).slice(-2);
-    $: seconds = ('0' + timeLeft % 60).slice(-2);
+  export let durationSeconds: number = 60 * 25; // Default is 25mins
+  export let clockName: ClockName;
+  let timeSpent = 0;
+  let playing: boolean = false;
 
-    const dispatch = createEventDispatcher<ClockEvents>();
+  let interval: number | undefined = undefined;
 
-    // TODO: Declare event type
-    const clock_complete = (was_manual_reset: boolean) => {
-        dispatch('clock_complete', {
-            totalDuration: durationSeconds,
-            duration: timeSpent,
-            clock: clockName,
-            was_manual_reset: was_manual_reset
-        })
-    };
-  
-    function startClock() {
-        interval = setInterval(() => {
-            timeSpent++;
-            if (timeSpent == durationSeconds) {
-                clock_complete(false);
-                stopClock();
-            }
-        }, 1000);
+  $: timeLeft = durationSeconds - timeSpent;
+  $: minutes = ("0" + Math.floor(timeLeft / 60)).slice(-2);
+  $: seconds = ("0" + (timeLeft % 60)).slice(-2);
+
+  const dispatch = createEventDispatcher<ClockEvents>();
+
+  // TODO: Declare event type
+  const clock_complete = (was_manual_reset: boolean) => {
+    dispatch("clock_complete", {
+      totalDuration: durationSeconds,
+      duration: timeSpent,
+      clock: clockName,
+      was_manual_reset: was_manual_reset,
+    });
+  };
+
+  function startClock() {
+    interval = setInterval(() => {
+      timeSpent++;
+      if (timeSpent == durationSeconds) {
+        clock_complete(false);
+        stopClock();
+      }
+    }, 1000);
+  }
+
+  function stopClock() {
+    if (interval !== undefined) {
+      clearInterval(interval);
+      interval = undefined;
     }
+    timeSpent = 0;
+    playing = false;
+  }
 
-    function stopClock() {
-        if (interval !== undefined) {
-            clearInterval(interval);
-            interval = undefined;
-        }
-        timeSpent = 0;
-        playing = false;
+  function handleClockFlip() {
+    if (playing) {
+      clock_complete(true);
+      stopClock();
+    } else {
+      startClock();
     }
+    playing = !playing;
+  }
 
-    function handleClockFlip() {
-        if (playing) {
-            clock_complete(true);
-            stopClock();
-        } else {
-            startClock();
-        }
-        playing = !playing;
-    }
+  onDestroy(stopClock);
+</script>
 
-    onDestroy(stopClock);
-
-  </script>
-  
-  
 <div class="flex flex-col items-center justify-center">
-    <div class="text-5xl pb-4">
-        {minutes}:{seconds}
-    </div>
-    <div class="text-xl">
-        <button on:click={handleClockFlip}>
-            {#if playing}
-                <Icon name="refresh-ccw"/>
-            {:else}
-                <Icon name="play"/>
-            {/if}
-        </button>
-    </div>
+  <div class="text-5xl pb-4">
+    {minutes}:{seconds}
+  </div>
+  <div class="text-xl">
+    <button on:click={handleClockFlip}>
+      {#if playing}
+        <Icon name="refresh-ccw" />
+      {:else}
+        <Icon name="play" />
+      {/if}
+    </button>
+  </div>
 </div>
-  
--- src/types/clock.ts
+++ src/types/clock.ts
@@ -1,16 +1,15 @@
-export type ClockName = 'focus_time' | 'short_pause' | 'long_pause' | 'dev_clock';
+export type ClockName = "focus_time" | "short_pause" | "long_pause";
 
 export type ClockInfo = {
-    name: ClockName,
-    durationSeconds: number
+  name: ClockName;
+  durationSeconds: number;
 };
 
-
 export type ClockEvents = {
-    clock_complete: {
-        totalDuration: number,
-        duration: number,
-        clock: ClockName,
-        was_manual_reset: boolean
-    }
-}
+  clock_complete: {
+    totalDuration: number;
+    duration: number;
+    clock: ClockName;
+    was_manual_reset: boolean;
+  };
+};
